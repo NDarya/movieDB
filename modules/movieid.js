a211982b0f84ee7cb756b063f2746f7ac651c63e
DaryaN <daryanotchenko@gmail.com>
2023-05-06 17:34:02 +0500

    classwork edits added

--- modules/movieid.js
+++ modules/movieid.js
@@ -1,101 +1,115 @@
 import { headerCreate } from "./ui";
 import { getData } from "./http";
 let header = document.querySelector('header')
-let iframe = document.querySelector('iframe')
+let body = document.body
+let movie_id = location.search.split('=').at(-1)
 
 headerCreate(header)
 
-let movie_id = location.search.split('=').at(-1)
-
-getData(`/movie/popular/${movie_id}`)
+getData('/movie/popular')
     .then(res => {
-        console.log(res.data);
-        let movName = document.querySelector('h1')
-        movName.innerHTML = item.title
+        // console.log(res);
     })
-
-getData(`/movie/${movie_id}/videos`)
-    .then(res => {
-        let video = res.data.results[Math.floor(Math.random() * res.data.results.length)]
-        iframe.src = `https://www.youtube.com/embed/${video.key}`;
-    })
-
-    
-let name_movie = document.querySelector(".name_mov")
-let h1 = document.querySelector("h1")
-let status = document.querySelector(".status")
-let p = document.querySelector("p")
-let img = document.querySelector(".head img")
-
-let year = document.querySelector(".year")
-let country = document.querySelector(".country")
-let tagline = document.querySelector(".tagline")
-let director = document.querySelector(".director")
-let scenario = document.querySelector(".scenario")
-let producer = document.querySelector(".producer")
-let operator = document.querySelector(".operator")
-let composer = document.querySelector(".composer")
-let poetry = document.querySelector(".poetry")
-let mon = document.querySelector(".mon")
-let genre = document.querySelector(".ganre")
-let world = document.querySelector(".world")
-let primer = document.querySelector(".primer")
-let prim = document.querySelector(".prim")
-let age = document.querySelector(".age")
-
-let actor_box = document.createElement('div')
-let actor_photo = document.createElement('img')
-let actor_name = document.createElement('p')
-let en_name = document.createElement('span')
-let actors = document.querySelector('.actors_grid')
 
 
 getData(`/movie/${movie_id}`)
-.then(res => {console.log(res)
-    name_movie.innerHTML = res.data.original_title
-    h1.innerHTML = res.data.title
-    status.innerHTML = res.data.tagline
-    p.innerHTML = res.data.overview
-    img.src = import.meta.env.VITE_BASE_IMG + res.data.poster_path
-    year.innerHTML = res.data.release_date
-    country.innerHTML = res.data.production_countries[0].name                                 
-    tagline.innerHTML = res.data.tagline
-    genre.innerHTML = res.data.genres[0].name
-    world.innerHTML = res.data.release_date
-    prim.innerHTML = res.data.release_date
-    age.innerHTML = res.data.release_date
-    primer.innerHTML = res.data.release_date
-    
-})
+    .then(({ data }) => {
+        body.style.backgroundImage = `url(${import.meta.env.VITE_BASE_IMG + data.backdrop_path})`
 
-function reload(data) {
-    
+        let poster = document.querySelector('.first-section__poster')
+        let loc = document.querySelector('.first-section__location-item_active')
+        let title = document.querySelector('.first-section__title')
+        let sub_title = document.querySelector('.first-section__sub-title')
+        let desrc = document.querySelector('.first-section__txt')
+        let rating = document.querySelector('.first-section__header-bottom-rating')
+        let fav = document.querySelector('.first-section__header-bottom-txt')
+        let rating_view = data.vote_average.toString().replaceAll('.', '').slice(0, 2)
+        let year = document.querySelector('#year')
+        let country = document.querySelector('#country')
+        let tagline = document.querySelector('#tagline')
+        let director = document.querySelector('#director')
+        let scenario = document.querySelector('#scenario')
+        let producer = document.querySelector('#producer')
+        let operator = document.querySelector('#operator')
+        let composer = document.querySelector('#composer')
+        let art = document.querySelector('#art')
+        let visual_effect = document.querySelector('#visual_effect')
+        let genre = document.querySelector('#ganre')
+        let profit = document.querySelector('#profit')
+        let premiere_world = document.querySelector('#premiere_world')
+        let premiere_RF = document.querySelector('#premiere_RF')
+        let age_limit = document.querySelector('#age_limit')
+        let duration = document.querySelector('#duration')
 
-    
-    }
+        loc.innerHTML = data.title
+        title.innerHTML = data.original_title
+        sub_title.innerHTML = data.original_title
+        desrc.innerHTML = data.overview
+        rating.firstElementChild.innerHTML = `Рейтинг ожиданий ${rating_view}%`
+        fav.innerHTML = `В избранном у ${Math.round(data.popularity)} человек`
+        year.innerHTML = data.release_date.split('-').at(0)
+        country.innerHTML = ''
+        tagline.innerHTML = data.title.split('.').at(0)
+        reloadMovieCrews(data.production_countries, country)
+        reloadMovieCrews(data.genres, genre)
+        profit.innerHTML = data.revenue + '$'
+        premiere_world.innerHTML = data.release_date
+        premiere_RF.innerHTML = data.release_date
+        if (data.adult === false) {
+            age_limit.innerHTML = '16+'
+        } else {
+            age_limit.innerHTML = '18+'
+        }
+        duration.innerHTML = `${data.runtime} мин.`
 
-getData(`/movie/${movie_id}/credits`)
-    .then(res =>{ console.log((data));
-        director.innerHTML = data.crew[1].name
-    scenario.innerHTML = data.crew[1].name
-    producer.innerHTML = data.crew[3].name
-    operator.innerHTML = data.crew[0].name
-    composer.innerHTML = data.crew[1].name
-    poetry.innerHTML = data.crew[1].name
-    mon.innerHTML = data.crew[1].name
-    actor_photo.src = import.meta.env.VITE_BASE_IMG + res.data.cast.profile_path
-    actor_name.innerHTML = res.data.cast.original_name
+        getData(`/movie/${movie_id}/credits`)
+            .then(({ data }) => {
+                let directors = data.crew.filter(el => el.known_for_department === 'Directing')
+                let scenarios = data.crew.filter(el => el.known_for_department === 'Writing')
+                let producers = data.crew.filter(el => el.known_for_department === 'Production')
+                let operators = data.crew.filter(el => el.known_for_department === 'Visual Effects')
+                let composers = data.crew.filter(el => el.known_for_department === 'Sound')
+                let arts = data.crew.filter(el => el.known_for_department === 'Art')
+                let visual_effects = data.crew.filter(el => el.known_for_department === 'Visual Effects')
+                reloadMovieCrews(directors, director)
+                reloadMovieCrews(scenarios, scenario)
+                reloadMovieCrews(producers, producer)
+                reloadMovieCrews(operators, operator)
+                reloadMovieCrews(composers, composer)
+                reloadMovieCrews(arts, art)
+                reloadMovieCrews(visual_effects, visual_effect)
+            })
 
+        rating.lastElementChild.style.width = `${rating_view}%`
+        poster.src = import.meta.env.VITE_BASE_IMG + data.poster_path
+
+        console.log(data);
     })
 
+
+function reloadMovieCrews(arr, place) {
+    place.innerHTML = ''
+    arr.forEach((el, idx) => {
+        if (idx !== arr.length - 1) {
+            if (el.name !== '' || false) {
+                place.innerHTML += `${el.name}, `
+            } else {
+                place.innerHTML += 'Никого не найдено!'
+            }
+        } else {
+            if (el.name !== '' || false) {
+                place.innerHTML += `${el.name}`
+            } else {
+                place.innerHTML += 'Никого не найдено!'
+            }
+        }
+    })
+}
 
 getData(`/movie/${movie_id}/videos`)
-    .then(res => { 
+    .then(res => {
         let video = res.data.results[Math.floor(Math.random() * res.data.results.length)]
-        iframe.src = `https://www.youtube.com/embed/${video.key}`;
-        
     })
-    actor_box.append(actor_photo, actor_name)
-    actors.append(actor_box)
 
-
+getData(`/movie/${movie_id}/credits`)
+    .then(res => console.log(res))
--- modules/ui.js
+++ modules/ui.js
@@ -1,9 +1,13 @@
+import { getData } from './http';
+
 export function headerCreate(place) {
 	place.innerHTML = ''
 	place.innerHTML = `
 		<div class="left">
-			<img src="/public/images/logo.svg" alt="image">
-			<img src="/public/icons/menu.svg" alt="image">
+			<a href='/'>
+				<img src="/public/images/logo.svg" alt="image">
+			</a>
+			<img src="/public/images/menu.svg" alt="image">
 		</div>
 		<nav>
 			<a href="#">Афиша</a>
@@ -21,171 +25,86 @@ export function headerCreate(place) {
 			<button>Войти</button>
 		</div>
     `
+	
 }
 
+let genres
+
+getData('/genre/movie/list')
+	.then(({ data }) => genres = data.genres)
 
 export function reload(arr, place) {
-    place.innerHTML = ''
-    for (let item of arr) {
-        place.innerHTML += `
-            <div class="movie-card" >
-                <div class="image">
-                    <img src="${import.meta.env.VITE_BASE_IMG + item.poster_path}" alt="image">
-                    <span>${item.vote_average}</span>
+	place.innerHTML = ''
+	for (let item of arr) {
+		let genre_str = ''
+		for (let genre of genres) {
+			for (let id of item.genre_ids) {
+				if(id === genre.id){
+					genre_str += ` ${genre.name},`
+				}
+			}
+		}
+		place.innerHTML += `
+			<div class="movie-card" >
+				<div class="image">
+					<img src="${import.meta.env.VITE_BASE_IMG + item.poster_path}" alt="image">
+					<span>${item.vote_average}</span>
 					<a href="/pages/movieid.html?id=${item.id}">
-                    	<button>Карточка фильма</button>
+						<button>Карточка фильма</button>
 					</a>
-                </div>
-                <div class="name__genre">
-                    <p>${item.title}</p>
-                    <p>${item.genre_ids}</p>
-                </div>
-            </div>
-	// 		<div class="modal hide" data-close id="modal_login" >
-    //     	<div class="modal__dialog">
-    //         <div class="modal__content" data-tabs="0" >
-    //             <form action="#" name="signup" >
-    //                 <div data-close class="modal__close">&times;</div>
-    //                 <div class="modal__title">Войти</div>
-    //                 <input required placeholder="Логин, почта или телефон" name="name" type="text" class="modal__input">
-    //                 <input required placeholder="Ваш пароль" name="surname" type="text" class="modal__input">
-    //                 <input required placeholder="Ваш пароль" name="password" type="password" class="modal__input">
-    //                 <button type="submit" class="btn btn_yellow">Войти</button>
-	// 				<button class="btn btn_dark" data-reg >Зарегистрироваться</button>
-	// 				<a href="#">Восстановить пароль</a>
-	// 				<img src="./public/icons/menu.svg" alt="">
-    //             </form>
-    //         </div>
-	// 		<div class="modal__content hide" data-tabs="1" >
-    //             <form action="#" name="signin" >
-    //                 <div data-close class="modal__close">&times;</div>
-    //                 <div class="modal__title">Зарегистрироваться</div>
-    //                 <input required placeholder="email" name="email" type="email" class="modal__input">
-	// 				<input required placeholder="Придумайте пароль" name="password" type="password" class="modal__input">
-					
-					
-    //                	<button class="btn btn_dark" data-log >Войти</button>
-
-	// 				<button type="submit" class="btn btn_yellow">Зарегистрироваться</button>
-					
-    //             </form>
-    //         </div>
-    //     </div>
-    // </div>
-	// 		<div class="modal hide">
-    //     	<div class="modal__dialog">
-    //         <div class="modal__content">
-    //             <form action="#">
-    //                 <div data-close class="modal__close"></div>
-    //                 <div class="modal__title">Зарегистрироваться</div>
-    //                 <input required placeholder="Имя" name="name" type="text" class="modal__input">
-    //                 <input required placeholder="Фамилия" name="surname" type="text class="modal__input">
-	// 				<input required placeholder="Придумайте логин" name="cr_login" type="phone" class="modal__input">
-	// 				<input required placeholder="Придумайте пароль" name="password" type="phone" class="modal__input">
-	// 				<input required placeholder="Повторите пароль" name="rep_password" type="phone" class="modal__input">
-	// 				<input required placeholder="Номер телефона или e-mail" name="email" type="email" class="modal__input">
-                   
-
-	// 				<ul>
-	// 					<li>
-	// 						<input type="radio" name="radio" id="">Соглашаюсь на условия политики конфиденциальности
-	// 					</li>
-	// 				<li>
-	// 					<input type="radio" name="radio" id="">Соглашаюсь на обработку персональных данных
-	// 				</li>
-                    
-	// 			</ul>
-	// 				<button class=" btn btn_yellow">Зарегистрироваться</button>
-					
-    //             </form>
-    //         </div>
-    //     </div>
-    // </div>
-    //         `
-
-	// let login_btn = document.querySelector('.log')
-	// let modal = document.querySelector('#modal_login')
-	// let close_modal = document.querySelectorAll('[data-close]')
-	// let modal__content = modal.querySelectorAll('.modal__content')
-	// let data_reg_btn = document.querySelector('[data-reg]')
-	// let data_log_btn = document.querySelector('[data-log]')
-	// let signUp_form = document.forms.signup
-	// let signIn_form = document.forms.signin
-	
-	// signUp_form.onsubmit = (e) => {
-	// 	e.preventDefault()
-
-	// 	let user = {}
-
-	// 	let fm = new FormData(signUp_form)
-
-	// 	fm.forEach((value, key) => {
-	// 		user[key] = value
-	// 	})
-
-	// 	signUp(user)
-	// 		.then(res => console.log(res))
-	// }
-
-	// signIn_form.onsubmit = (e) => {
-	// 	e.preventDefault()
-
-	// 	let user = {}
-
-	// 	let fm = new FormData(signIn_form)
-
-	// 	fm.forEach((value, key) => {
-	// 		user[key] = value
-	// 	})
-
-	// 	signIn(user)
-	// 		.then(res => console.log(res))
-	// }
-
-	// data_reg_btn.onclick = () => {
-	// 	modal__content[1].classList.remove('hide')
-	// 	modal__content[0].classList.add('hide')
-	// }
-	// data_log_btn.onclick = () => {
-	// 	modal__content[0].classList.remove('hide')
-	// 	modal__content[1].classList.add('hide')
-	// }
-
-	// login_btn.onclick = () => {
-	// 	modal.classList.remove('hide')
-	// }
-
-	// close_modal.forEach(btn => {
-	// 	btn.onclick = (e) => {
-	// 		let isCloseElement = e.target.getAttribute('data-close') ?? "there is no data-attribute"
-
-	// 		if(!isCloseElement) {
-	// 			modal.classList.add('hide')
-	// 		}
-	// 	}
-	// })
-
+				</div>
+				<div class="name__genre">
+					<p>${item.title}</p>
+					<p>${genre_str}</p>
+				</div>
+			</div>
+			`
+	}
 }
 
-place.innerHTML = ''
-    for (let item of arr) {
-        place.innerHTML += `
-            <div class="movie-card" >
-                <div class="image">
-                    <img src="${import.meta.env.VITE_BASE_IMG + item.poster_path}" alt="image">
-                    <span>${item.vote_average}</span>
-					<a href="/pages/movieid.html?id=${item.id}">
-                    	<button>Карточка фильма</button>
-					</a>
-                </div>
-                <div class="name__genre">
-                    <p>${item.title}</p>
-                    <p>${item.genre_ids}</p>
-                </div>
-            </div>
-            `
-    }
+export function reloadTrailerCart(arr, place) {
+	place.innerHTML = ''
+	for (let item of arr) {
+		place.innerHTML += `
+		<div class="trailers__footer-item" data-id='${item.id}'>
+			<div class="trailers__vid trailers__vid_mini">
+				<img class="trailers__iframe trailers__iframe_small" src="${import.meta.env.VITE_BASE_IMG + item.poster_path}" alt='image'>
+				<img class="trailers_play-icon trailers_play-icon_mini" src="/public/icons/play.svg" alt="icon">
+			</div>
+			<p class="trailers__title trailers__title_small">${item.title.split('.').at(0)}</p>
+		</div>
+		`
+	}
 }
 
-
-
+export function reloadPopularPerson(arr, place) {
+	place.innerHTML = ''
+	if (arr.length === 2) {
+		for (let item of arr) {
+			place.innerHTML += `
+			<div class="popular-persons__box">
+				<img class="popular-persons__photo" src="${import.meta.env.VITE_BASE_IMG + item.profile_path}" alt="image">
+				<div class="popular-persons__num">${arr.indexOf(item) + 1}-е место</div>
+				<div class="popular-persons__box-item">
+					<div class="popular-persons__name">${item.name}</div>
+					<div class="popular-persons__name_eng">${item.name}</div>
+					<div class="popular-persons__age">${item.age}</div>
+				</div>
+			</div>
+			`
+		}
+	} else {
+		for (let item of arr) {
+			place.innerHTML += `
+		<div class="popular-persons__cart">
+			<div class="popular-persons__cart-item">
+				<div class="popular-persons__name">${item.name}</div>
+				<div class="popular-persons__name_eng">${item.name}</div>
+				<div class="popular-persons__age">${item.age}</div>
+			</div>
+			<div class="popular-persons__num">${arr.indexOf(item) + 3}-е место</div>
+		</div>
+		`
+		}
+	}
+}
